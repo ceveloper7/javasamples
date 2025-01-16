4cf4739bb19d422e3126ea3dbdde00900eab3888
ceveloper7 <cevabarcvilla@gmail.com>
2025-01-16 18:42:43 -0300

    JUnit Chapter 8 - Unit Test Refactoring with Mock Objects

---
+++ junit-in-action/src/main/java/com/ceva/ch08/configurations/DefaultConfiguration.java
@@ -0,0 +1,20 @@
+package com.ceva.ch08.configurations;
+
+public class DefaultConfiguration implements Configuration{
+    /**
+     * Constructor.
+     *
+     * @param configurationName
+     */
+    public DefaultConfiguration(String configurationName) {
+    }
+
+    /**
+     * Getter method to get the sql that we want to execute.
+     *
+     * @return
+     */
+    public String getSQL(String sqlString) {
+        return null;
+    }
+}
---
+++ junit-in-action/src/test/java/com/ceva/ch08/configurations/MockConfiguration.java
@@ -0,0 +1,21 @@
+package com.ceva.ch08.configurations;
+
+public class MockConfiguration implements Configuration{
+    /**
+     * Sets the sql query.
+     *
+     * @param sqlString
+     */
+    public void setSQL(String sqlString) {
+    }
+
+    /**
+     * Gets the sql query.
+     *
+     * @param sqlString
+     * @return
+     */
+    public String getSQL(String sqlString) {
+        return null;
+    }
+}

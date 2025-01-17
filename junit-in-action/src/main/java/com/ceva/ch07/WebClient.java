ecbdb7b954de27456743f625d9d48122861f0142
ceveloper7 <cevabarcvilla@gmail.com>
2025-01-16 21:44:44 -0300

    JUnit Chapter 8 - Unit Test Refactoring with Mock Objects - Easy Refactoring Technique

--- junit-in-action/src/main/java/com/ceva/ch07/WebClient.java
+++ junit-in-action/src/main/java/com/ceva/ch07/WebClient.java
@@ -13,13 +13,13 @@ public class WebClient {
     public String getContent(URL url) {
         StringBuffer content = new StringBuffer();
         try {
-            // 1. abrimos la conexion
+            // 1. abrimos la conexion HTTP
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setDoInput(true);
             InputStream is = connection.getInputStream();
             byte[] buffer = new byte[2048];
             int count;
-            // 2. procesamos el stream hasta terminar el contenido
+            // 2. procesamos el stream hasta terminar el contenido recibido
             while (-1 != (count = is.read(buffer))) {
                 content.append(new String(buffer, 0, count));
             }

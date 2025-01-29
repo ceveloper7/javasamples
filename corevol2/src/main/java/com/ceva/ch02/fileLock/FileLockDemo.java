d9a2360d523e4efdea3d1543ecd12e0914311660
Carlos V. <cevabarcvilla@gmail.com>
2025-01-29 20:53:11 -0300

    Java core Vol II - Chapter 2 Input/output - Working with files

---
+++ corevol2/src/main/java/com/ceva/ch02/fileLock/FileLockDemo.java
@@ -0,0 +1,72 @@
+package com.ceva.ch02.fileLock;
+
+import java.io.*;
+import java.nio.*;
+import java.nio.channels.*;
+import java.nio.file.*;
+import java.security.*;
+import java.security.spec.*;
+import java.util.*;
+
+import javax.crypto.*;
+import javax.crypto.spec.*;
+
+public class FileLockDemo {
+    public static void main(String[] args) throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
+        Path path = Path.of("password.properties");
+        FileChannel channel = FileChannel.open(path, StandardOpenOption.READ, StandardOpenOption.WRITE, StandardOpenOption.CREATE);
+        // bloqueo del archivo
+        try(FileLock lock = channel.lock()){
+            // Read bytes from file
+            ByteBuffer readBuffer = ByteBuffer.allocate((int) channel.size());
+            channel.read(readBuffer);
+
+            // Read props from bytes
+            Reader in = new StringReader(new String(readBuffer.array()));
+            Properties props = new Properties();
+            props.load(in);
+
+            // Get username, password from args or user input
+            String username;
+            char[] password;
+            if (args.length >= 2)
+            {
+                username = args[0];
+                password = args[1].toCharArray();
+            }
+            else
+            {
+                Console console = System.console();
+                username = console.readLine("User name: ");
+                password = console.readPassword("Password: ");
+            }
+
+            // Hash password
+            final int ITERATIONS = 210_000;
+            final int SALT_BYTES = 16;
+            final int HASH_BYTES = 32;
+            SecureRandom random = new SecureRandom();
+            byte[] salt = new byte[SALT_BYTES];
+            random.nextBytes(salt);
+
+            var spec = new PBEKeySpec(password, salt, ITERATIONS, 8 * HASH_BYTES);
+            SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+            byte[] hash = skf.generateSecret(spec).getEncoded();
+            Base64.Encoder encoder = Base64.getEncoder();
+
+            // Put username and hashed password into props
+            props.put(username, ITERATIONS + "|" + encoder.encodeToString(salt) + "|"
+                    + encoder.encodeToString(hash));
+
+            // Write props to bytes
+            StringWriter out = new StringWriter();
+            props.store(out, null);
+            byte[] outBytes = out.toString().getBytes();
+
+            // Write bytes to file
+            channel.truncate(0);
+            ByteBuffer writeBuffer = ByteBuffer.wrap(outBytes);
+            channel.write(writeBuffer);
+        }
+    }
+}

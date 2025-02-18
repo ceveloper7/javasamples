dcbe28bde781f4c204279018665a3b2dd2ad0533
Carlos V. <cevabarcvilla@gmail.com>
2025-02-17 22:42:12 -0300

    Java Core Volumen II - Chapter 10 - Graphical User Interface Programming - Displaying information in a Component

---
+++ corevol2/src/main/java/com/ceva/ch10/component/NotHelloWorldComponent.java
@@ -0,0 +1,25 @@
+package com.ceva.ch10.component;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * A component that displays a message.
+ */
+public class NotHelloWorldComponent extends JComponent {
+    public static final int MESSAGE_X = 75;
+    public static final int MESSAGE_Y = 100;
+
+    private static final int DEFAULT_WIDTH = 300;
+    private static final int DEFAULT_HEIGHT = 200;
+
+    public void paintComponent(Graphics g)
+    {
+        g.drawString("Not a Hello, World program", MESSAGE_X, MESSAGE_Y);
+    }
+
+    public Dimension getPreferredSize()
+    {
+        return new Dimension(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+    }
+}
---
+++ corevol2/src/main/java/com/ceva/ch10/component/ShowInfoComponent.java
@@ -0,0 +1,29 @@
+package com.ceva.ch10.component;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ShowInfoComponent {
+    public static void main(String[] args)
+    {
+        EventQueue.invokeLater(() ->
+        {
+            var frame = new NotHelloWorldFrame();
+            frame.setTitle("NotHelloWorld");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setVisible(true);
+        });
+    }
+}
+
+/**
+ * A frame that contains a message panel.
+ */
+class NotHelloWorldFrame extends JFrame
+{
+    public NotHelloWorldFrame()
+    {
+        add(new NotHelloWorldComponent());
+        pack();
+    }
+}

be42afba496b0eeec25ed50d8083b7ff2a007821
Carlos V. <cevabarcvilla@gmail.com>
2025-02-22 20:57:56 -0300

    Java Core Volume II - Chapter 11 - UI Components with Swing - ComboBox Test

---
+++ corevol2/src/main/java/com/ceva/ch11/comboBox/ComboBoxTest.java
@@ -0,0 +1,19 @@
+package com.ceva.ch11.comboBox;
+
+
+import java.awt.*;
+import javax.swing.*;
+
+public class ComboBoxTest
+{
+    public static void main(String[] args)
+    {
+        EventQueue.invokeLater(() ->
+        {
+            var frame = new ComboBoxFrame();
+            frame.setTitle("ComboBoxTest");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setVisible(true);
+        });
+    }
+}

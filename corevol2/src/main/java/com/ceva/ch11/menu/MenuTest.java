bed16e4a6a77ee831e346332096ba6dab7143d5b
Carlos V. <cevabarcvilla@gmail.com>
2025-02-23 16:50:42 -0300

    Java Core Volume II - Chapter 11 - UI Swing components - Menu & MenuItems

---
+++ corevol2/src/main/java/com/ceva/ch11/menu/MenuTest.java
@@ -0,0 +1,18 @@
+package com.ceva.ch11.menu;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class MenuTest
+{
+    public static void main(String[] args)
+    {
+        EventQueue.invokeLater(() ->
+        {
+            var frame = new MenuFrame();
+            frame.setTitle("MenuTest");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setVisible(true);
+        });
+    }
+}

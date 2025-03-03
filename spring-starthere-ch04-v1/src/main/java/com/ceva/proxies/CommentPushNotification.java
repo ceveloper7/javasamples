cfb41493eccd7c5e1b83f816bf4099d88089c656
Carlos V. <cevabarcvilla@gmail.com>
2025-03-03 18:28:00 -0300

    Spring Start Here - Chapter 4 - Spring Context - Using dependency injection with Abstraction - Comment register App

---
+++ spring-starthere-ch04-v1/src/main/java/com/ceva/proxies/CommentPushNotification.java
@@ -0,0 +1,14 @@
+package com.ceva.proxies;
+
+import com.ceva.model.Comment;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Primary
+public class CommentPushNotification implements CommentNotificationProxy{
+    @Override
+    public void sendComment(Comment comment) {
+        System.out.println("Sending push notification for comment: " + comment.getText());
+    }
+}

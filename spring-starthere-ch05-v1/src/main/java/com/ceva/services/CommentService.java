8ae534b4eaec8d2dc23133fbaa1b7551ea39b58e
Carlos V. <cevabarcvilla@gmail.com>
2025-03-04 17:49:15 -0300

    Spring Start here - Chapter 5 - Injecting Singleton Bean

---
+++ spring-starthere-ch05-v1/src/main/java/com/ceva/services/CommentService.java
@@ -0,0 +1,20 @@
+package com.ceva.services;
+
+import com.ceva.repositories.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CommentService {
+
+    private final CommentRepository commentRepository;
+
+    @Autowired
+    public CommentService(CommentRepository commentRepository){
+        this.commentRepository = commentRepository;
+    }
+
+    public CommentRepository getCommentRepository(){
+        return commentRepository;
+    }
+}
--- spring-starthere-ch05/src/main/java/com/ceva/Main.java
+++ spring-starthere-ch05/src/main/java/com/ceva/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) {
 
         var context = new AnnotationConfigApplicationContext(ProjectConfiguration.class);
-        // get singleton beans
+        // get singleton beans. aunque no es necesario indicar el nombre del bean ya que solo hay un tipo de bean CommentService, pero se puede hacer.
         var cs1 = context.getBean("commentService", CommentService.class);
         var cs2 = context.getBean("commentService", CommentService.class);
 

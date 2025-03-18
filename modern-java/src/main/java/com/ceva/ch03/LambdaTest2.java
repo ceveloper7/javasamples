c47c514136dc4c8bf375e3274b6d512c06100228
Carlos V. <cevabarcvilla@gmail.com>
2025-03-18 20:49:32 -0300

    Moder Java 8 - Chapter 2 - Supplier, Consumer, Function

--- modern-java/src/main/java/com/ceva/ch03/LambdaTest2.java
+++ modern-java/src/main/java/com/ceva/ch03/LambdaTest2.java
@@ -4,8 +4,10 @@ import com.ceva.model.Apple;
 import com.ceva.model.Color;
 import com.ceva.model.Util;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class LambdaTest2 {
@@ -22,9 +24,15 @@ public class LambdaTest2 {
         Util.forEach(Apple.appleInventory(), appleData);
     }
 
+    private void printDataUsingFunction(){
+        List<Integer> data = Util.map(Arrays.asList("Lambdas", "in", "action"), (String s) -> s.length());
+        System.out.println(data);
+    }
+
     public static void main(String[] args) {
         LambdaTest2 t = new LambdaTest2();
         // t.printRedApplesUsingPredicate();
         t.printAppleDataUsingConsumer();
+        t.printDataUsingFunction();
     }
 }
--- modern-java/src/main/java/com/ceva/model/Util.java
+++ modern-java/src/main/java/com/ceva/model/Util.java
@@ -3,6 +3,7 @@ package com.ceva.model;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 public final class Util {
@@ -20,5 +21,13 @@ public final class Util {
         for(T t : list){
             c.accept(t);
         }
+    }
+
+    public static <T,R> List<R> map(List<T> list, Function<T, R> f){
+        List<R> results = new ArrayList<>();
+        for (T t : list){
+            results.add(f.apply(t));
+        }
+        return results;
     }
 }

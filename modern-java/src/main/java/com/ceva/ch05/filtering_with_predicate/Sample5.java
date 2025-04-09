2a2c070c32c24f5718c9c78cdda4c96dc2f34052
Carlos V. <cevabarcvilla@gmail.com>
2025-04-09 20:07:01 -0300

    Moder Java - Chapter 5 - Skipping elemets in a stream with skip(n)

---
+++ modern-java/src/main/java/com/ceva/ch05/filtering_with_predicate/Sample5.java
@@ -0,0 +1,24 @@
+package com.ceva.ch05.filtering_with_predicate;
+
+import com.ceva.model.Dish;
+
+import java.util.List;
+
+/**
+ * Skipping stream elements
+ */
+public class Sample5 {
+    public static void main(String[] args) {
+        List<Dish> menu = Dish.crearListaDePlatos();
+
+        /**
+         * Saltamos los dos primeros platos del stream que contiene platos que superan las 800 calorias
+         */
+        List<Dish> skip = menu.stream()
+                .filter(dish -> dish.getCalories() > 800)
+                .skip(2)
+                .toList();
+
+        skip.forEach(System.out::println);
+    }
+}

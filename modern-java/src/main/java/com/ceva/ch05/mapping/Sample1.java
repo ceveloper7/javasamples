ab3e75911c747bc37a0683e94641747e31de89cb
Carlos V. <cevabarcvilla@gmail.com>
2025-04-09 20:26:26 -0300

    Moder Java - Chapter 5 - Applying a function to each elemento of a Stream with map()

---
+++ modern-java/src/main/java/com/ceva/ch05/mapping/Sample1.java
@@ -0,0 +1,21 @@
+package com.ceva.ch05.mapping;
+
+import com.ceva.model.Dish;
+
+import java.util.List;
+
+/**
+ * mapping
+ */
+public class Sample1 {
+    public static void main(String[] args) {
+        List<Dish> menu = Dish.crearListaDePlatos();
+
+        // obtenemos todos los nombres de los platos
+        List<String> dishNames = menu.stream()
+                .map(dish -> dish.getName())
+                .toList();
+
+        dishNames.forEach(System.out::println);
+    }
+}

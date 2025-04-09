46043717861ebfd801d91a739288930de7453f0e
Carlos V. <cevabarcvilla@gmail.com>
2025-04-09 20:00:45 -0300

    Moder Java - Chapter 5 - Truncating stream with limit()

---
+++ modern-java/src/main/java/com/ceva/ch05/filtering_with_predicate/Sample4.java
@@ -0,0 +1,23 @@
+package com.ceva.ch05.filtering_with_predicate;
+
+import com.ceva.model.Dish;
+
+import java.util.List;
+
+/**
+ * Truncating stream with limit operation
+ */
+public class Sample4 {
+    public static void main(String[] args) {
+
+        List<Dish> menu = Dish.crearListaDePlatos();
+
+        // seleccionar los 3 primeros platos que tienen mas de 300 calorias
+        List<Dish> dishesWithCaloriesGreaterThan300 = menu.stream()
+                .filter(dish -> dish.getCalories() > 300)
+                .limit(3)
+                .toList();
+
+        dishesWithCaloriesGreaterThan300.forEach(System.out::println);
+    }
+}

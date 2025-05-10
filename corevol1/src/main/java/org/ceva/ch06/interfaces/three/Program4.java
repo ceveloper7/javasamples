ae93e26f2f563d347c7df16c90dad887cada767a
Carlos V. <cevabarcvilla@gmail.com>
2025-05-09 21:42:15 -0300

    Java Core Vol 1 - Chapter 6 - Inner class - Anonymous Inner Classes

---
+++ corevol1/src/main/java/org/ceva/ch06/interfaces/three/Program4.java
@@ -0,0 +1,33 @@
+package org.ceva.ch06.interfaces.three;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.time.Instant;
+
+/**
+ * Inner class anonimas
+ */
+public class Program4 {
+    public void start(int interval, boolean beep){
+        // podemos tener inner class sin dar un nombre a la clase
+        var listener = new ActionListener(){
+            public void actionPerformed(ActionEvent event){
+                System.out.println("At the tone, the time is " + Instant.ofEpochMilli(event.getWhen()));
+                if(beep)
+                    Toolkit.getDefaultToolkit().beep();
+            }
+        };
+        var timer = new Timer(interval, listener);
+        timer.start();
+    }
+
+    public static void main(String[] args) {
+        Program4 program4 = new Program4();
+        program4.start(1000, true);
+
+        JOptionPane.showMessageDialog(null, "Quit program");
+        System.exit(0);
+    }
+}

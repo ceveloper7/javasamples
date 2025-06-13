e923781e2901d16c598dabc1555ebbac34f8c1b8
Carlos V. <cevabarcvilla@gmail.com>
2025-06-13 11:11:29 -0300

    Java Exercise Vol I

---
+++ java-exercise-vol1/src/main/java/com/ceva/ex1/ch04/arrays/loop/DiceGame.java
@@ -0,0 +1,83 @@
+package com.ceva.ex1.ch04.arrays.loop;
+
+import java.util.Arrays;
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * Enhanced for loop: Numbers well shuffled
+ */
+public class DiceGame {
+    // lanzar los dados
+    private static int[] shuffleDice(){
+        int[] result = new int[5];
+        for(int i = 0; i < result.length; i++){
+            result[i] = ThreadLocalRandom.current().nextInt(1,7);
+        }
+        return result;
+    }
+
+    private static boolean isHomogeneous(int[] values){
+        int first = values[0];
+        // enhanced loop
+        for(int value : values){
+            if(value != first)
+                return false;
+        }
+        return true;
+    }
+
+    private static int[] occurrences(int[] values){
+        int[] result = new int[6];
+        for(int value : values){
+            result[value - 1]++;
+        }
+        return result;
+    }
+
+    private static boolean isFullHouse(int[] values){
+        boolean hasTwoOfAKind = false;
+        boolean hasThreeOfAKind = false;
+
+        for(int occurence : occurrences(values)){
+            switch (occurence){
+                case 2 -> hasTwoOfAKind = true;
+                case 3 -> hasThreeOfAKind = true;
+                //default -> { return false;}
+            }
+        }
+        return hasThreeOfAKind && hasTwoOfAKind;
+    }
+    
+    private static void printDiceValues(int[] values){
+        int[]occurences = occurrences(values);
+
+        boolean first = true;
+        for(int value = 0; value < occurences.length; value++){
+            if(occurences[value] > 0){
+                System.out.printf("%s%s%c",
+                        first ? "" : ", ",
+                        times(occurences[value]),
+                        dieFace(value));
+                first = false;
+            }
+        }
+    }
+
+    private static String times(int value){
+        return value == 1 ? "" : value + " x ";
+    }
+
+    private static char dieFace(int value){
+        return new char[] {'\u2680', '\u2681', '\u2682', '\u2683', '\u2684', '\u2685'}[value];
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Lanzamos los dados: " + Arrays.toString(DiceGame.shuffleDice()));
+        System.out.println("Son iguales los numeros: " + isHomogeneous(new int[]{1, 1, 1, 1}));
+        System.out.println("Son iguales los numeros: " + isHomogeneous(new int[]{1, 1, 1, 2, 2 }));
+        System.out.println("La ocurrencia de numeros: " + Arrays.toString(DiceGame.occurrences(new int[]{1, 1, 2, 3, 4}))); // [2,1,1,1,0,0]
+        System.out.println("is Full house: " + isFullHouse(new int[]{1, 1, 1, 2, 2}));
+        System.out.println("is full house: " + isFullHouse(new int[]{1, 1, 1, 1}));
+        printDiceValues(new int[]{2,4,2,4,6});
+    }
+}

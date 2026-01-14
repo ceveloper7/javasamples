0ff0edec628a0ccfa3d4eebc49e966846ee5536b
Carlos V. <cevabarcvilla@gmail.com>
2026-01-14 19:40:18 -0300

    Algorithms - Selection Sort

---
+++ algorithms/src/main/java/com/algo/ch01/selectionsort/Test.java
@@ -0,0 +1,12 @@
+package com.algo.ch01.selectionsort;
+
+import java.util.Arrays;
+
+public class Test {
+    public static void main(String[] args) {
+        int[] numbers = {5,3,6,2,15};
+        Util.selectionSort(numbers);
+
+        System.out.println(Arrays.toString(numbers));
+    }
+}
---
+++ algorithms/src/main/java/com/algo/ch01/selectionsort/Util.java
@@ -0,0 +1,29 @@
+package com.algo.ch01.selectionsort;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Util {
+    private static int findSmallest(int[] arr, int startIndex){
+        int smallestIndex = startIndex;
+
+        for (int i = startIndex + 1; i < arr.length; i++) {
+            if (arr[i] < arr[smallestIndex]) {
+                smallestIndex = i;
+            }
+        }
+        return smallestIndex;
+    }
+
+    public static void selectionSort(int[] arr){
+        for (int i = 0; i < arr.length - 1; i++) {
+            int smallestIndex = findSmallest(arr, i);
+
+            // intercambio (swap)
+            int temp = arr[i];
+            arr[i] = arr[smallestIndex];
+            arr[smallestIndex] = temp;
+        }
+    }
+}
